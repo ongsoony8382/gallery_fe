d72472a85185616ed3fe609dd12517aace950107
ongsoony8382 <ongsoony8382@gmail.com>
2025-07-07 16:20:14 +0900

    초기

---
+++ src/main.js
@@ -0,0 +1,18 @@
+//import './assets/main.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+
+router.isReady().then(()=>{ //프로미스 객체의 주소값을 리턴 
+    app.mount('#app')
+});
+
+app.mount('#app')
